15d509d00088b8b467950083988b61855f947cf0
Jefferson Marques <jefferson.jsm3444@gmail.com>
2024-05-27 00:25:00 -0300

    Criando camada de infraestrutura

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,17 @@
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
+import { PrismaService } from '../prisma/prisma.service'
+import { Module } from '@nestjs/common'
+
+@Module({
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+    FetchRecentQuestionsController,
+  ],
+  providers: [PrismaService],
+})
+export class HttpModule {}
